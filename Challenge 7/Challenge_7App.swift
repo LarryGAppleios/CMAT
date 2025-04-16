22f95efaaa0fd4efa5609bce6219bbcb18f33cbb
Larry Gill <larrygill909@gmail.com>
2025-04-16 08:42:29 -0400

    Initial Commit

---
+++ Challenge 7/Challenge_7App.swift
@@ -0,0 +1,17 @@
+//
+//  Challenge_7App.swift
+//  Challenge 7
+//
+//  Created by Larry Gill on 4/16/25.
+//
+
+import SwiftUI
+
+@main
+struct Challenge_7App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Challenge 7Tests/Challenge_7Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Challenge_7Tests.swift
+//  Challenge 7Tests
+//
+//  Created by Larry Gill on 4/16/25.
+//
+
+import Testing
+@testable import Challenge_7
+
+struct Challenge_7Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
